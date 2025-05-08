fe6130bc00cfc7dd41b6e9f30ab9137eefcc09bb
jeffrey_strabala <166744338+strabalaj@users.noreply.github.com>
2025-05-08 14:57:10 -0700

    implemented signup function

--- src/Users/Users.ts
+++ src/Users/Users.ts
@@ -2,13 +2,15 @@ import User from "./User";
 import { normalize } from "path";
 import { readFile, writeFile } from "fs/promises";
 import * as fs from "fs";
+import crypto from "crypto";
+
 type Users_ = {
   Users: User[];
   Users_loc: User_Stats[];
 };
 
 type Pre_User_Stats = {
-  Username: String;
+  Username: string;
   email: string;
   password: string;
 };
@@ -116,14 +118,52 @@ export default class Users {
   /** ------------------------ */
 
   async signUp(Data: Pre_User_Stats): Promise<Status> {
-    let data: Status = {
-      return: 1,
-      args: [],
-      comments: "",
-    };
+  const data: Status = {
+    return: 1,
+    args: [],
+    comments: "",
+  };
 
+  //duplicate check using hte email and username
+  if (this.Users_email[Data.email] || this.Users_username[Data.Username]) {
+    data.comments = "There is already a user with that info.";
     return data;
   }
+
+  //salt and hash the password
+  const salt = crypto.randomBytes(16).toString("hex");
+  const hashed = crypto
+    .pbkdf2Sync(Data.password, salt, 1000, 64, "sha512")
+    .toString("hex");
+  const id = crypto.randomUUID();
+  const ref = crypto.randomBytes(8).toString("hex");
+
+  // create the User_Stats object
+  const userStats: User_Stats = {
+    Username: Data.Username,
+    email: Data.email,
+    hashed,
+    salt,
+    id,
+    ref,
+  };
+
+  // Create new user instance and store User_Stats that was populated
+  const user = new User(this.Path, userStats);
+  this.Users_system.push(userStats);
+  this.Users_data.push(user);
+  this.Users_username[Data.Username] = user;
+  this.Users_email[Data.email] = user;
+  this.Users_id[id] = user;
+  this.Users_ref[ref] = user;
+
+  await this.save();
+
+  data.return = 0;
+  data.args = [userStats];
+  data.comments = "Sign up successful.";
+  return data;
+}
 
   async login(login: Login): Promise<Status> {
     let data: Status = {
